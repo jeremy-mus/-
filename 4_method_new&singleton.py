adf4545840bed377d6f6d5210b897e00d6813fea
jeremy-mus <145211896+jeremy-mus@users.noreply.github.com>
2024-03-26 21:45:03 +0300

    Add files via upload

--- 4_method_new&singleton.py
+++ 4_method_new&singleton.py
@@ -13,10 +13,21 @@ pt = Point(1, 2)
 print(pt) # Будет None, тк не был создан экземпляр класса
 
 
-
+# Singleton
 
 
 class DataBase:
+
+    __isinstance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__isinstance == None:
+            cls.__isinstance = super().__new__(cls)
+        return cls.__isinstance
+    
+    def __del__(self):
+        DataBase.__isinstance = None
+
     def __init__(self, user, psw, port):
         self.user = user
         self.psw = psw
@@ -33,3 +44,12 @@ class DataBase:
     
     def write(self, data):
         print(f"Запись с БД: {data}")
+
+
+db1 = DataBase("Иван", "1234", 80)
+db2 = DataBase("Афанасий", "228337", 50)
+
+print(id(db1), id(db2), id(db1) == id(db2)) # При попытке создания второго экземпляра класса он не был создан, а просто ссылается на первый
+
+db1.connect()
+db2.connect()
